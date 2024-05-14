b21bca3192f507784001e1b72e5f7760a65b4ac6
emreipekci <153276209+emreipekci@users.noreply.github.com>
2024-05-14 09:51:22 +0200

    new project

---
+++ project.js
@@ -0,0 +1,13 @@
+/*When the user inputs a data
+initialize a computer choice data
+Compare these two data and give the score
+play this 5 rounds and print the result
+*/
+
+
+let choices = ["rock", "paper", "scissors"];
+let randomChoices = Math.floor(Math.random() * 2);
+console.log(randomChoices);
+function getComputerChoice () {
+    return 
+}
